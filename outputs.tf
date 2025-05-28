89ada37b3ea7e9fe2b2e7f79f3a24593ebaee3b7
Lucas Barboza <lucas_mendes@id.uff.br>
2025-05-28 15:41:26 -0300

    Adding possiblities

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,16 @@ output "address_full_map" {
     }
   }
 }
+
+output "address_full_map_csv" {
+  value = join("\n", [
+    "name,description,id,ip_address", # Cabeçalho do CSV
+    for obj in ciscomcd_address_object.address : 
+    join(",", [
+      obj.name,
+      obj.description,
+      obj.id,
+      join(";", tolist(obj.value)) # Une IPs (se houver mais de um) com ";"
+    ])
+  ])
+}
